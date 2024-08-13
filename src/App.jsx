1b516c52f0896366bff0f8e9ffc2bb47a761c422
alkesh <alkesh@alkeshs-MacBook-Air.local>
2024-08-14 00:18:05 +0530

    add aside

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AddScetion from './component/AddScetion'
 import CataCard from './component/CataCard'
 import Navbar from './component/Navbar'
 import { useEffect, useState } from 'react'
+import AddSide from './component/AddSide'
 // import { addWidgetToCategory } from './store/auth'
 
 function App() {
@@ -17,14 +18,24 @@ function App() {
   },[userData])
   console.log(userData)
 
+  const [show , setShow] = useState(false)
+  
+  const handleShowAside = () =>{
+    setShow(showprev => !showprev)
+    console.log("handleshow work")
+  }
+
   return (
     <div className='bg-gray-200 h-full pb-10 w-full relative'>
       <Navbar/>
-      <AddScetion/>
+      <AddScetion handleShowAside={handleShowAside}/>
       <div className='overflow-x-auto'>
         {items && items.map((items ) => (
             <CataCard items={items} key={items.id}/>
         ))}
+      </div>
+      <div className={`${show ? "" :"hidden"} absolute  top-0 right-0 h-full w-full sm:w-[600px]`}> 
+          <AddSide show={show} handleShowAside={handleShowAside}/>
       </div>
     </div>
   )
---
+++ src/component/AddSide.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {X} from "lucide-react"
+import { useState } from 'react'
+function AddSide({handleShowAside}) {
+
+    const [slide , setSlide] = useState('79px')
+    const [slidewidth , setslidewidth] = useState('40px')
+
+    const handleSlide = (newslide , newslidewidth) =>{
+        setSlide(newslide)
+        setslidewidth(newslidewidth)
+    }
+
+    // console.log(show)
+
+  return (
+    <div className={`h-full show w-full sm:w-full bg-white border-l border-black`}>
+        <div className='flex text-white bg-blue-500 py-3 justify-between items-center px-5'>
+              <div className='text-md font-medium '>add widget</div>
+            <div className='' onClick={()=> handleShowAside()}><X/></div>
+        </div>
+        <div className='px-5 py-4'>
+            personalise your dashboard by adding the following widget
+        </div>
+        <div className='px-5 border-b border-black'>
+            <div className='relative flex  pb-3 gap-5'>
+                    <button className=' font-medium text-sm'  onClick={()=> handleSlide('0px' , "40px")}>
+                            CSPM
+                    </button>
+                    <button className='font-medium text-sm'   onClick={()=> handleSlide('60px' , "40px")}>
+                         CWPP
+                    </button>
+                    <button className='font-medium text-sm' onClick={()=> handleSlide('120px' , "48px")}>
+                           Image
+                    </button>
+                    <button className='font-medium text-sm' onClick={()=> handleSlide('180px' , "48px")}>
+                           TIcket
+                    </button>
+                    
+                    <div className={`h-[2px] absolute top-[30px]  rounded-xl transition-all duration-200 bg-black `} style={{left : slide , width : slidewidth}}></div>
+                </div> 
+            </div>
+    </div>
+  )
+}
+
+export default AddSide
